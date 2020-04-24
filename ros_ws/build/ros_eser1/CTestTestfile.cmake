80809c1b6c11df4a67790f03a4e7a70a02df3c58
salvatore-punzo <salvatore.punzo95@gmail.com>
2020-04-24 23:45:57 +0200

    ros_ws

--- ros_ws/build/ros_eser1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/salvatore/ros_ws/src/ros_eser1
-# Build directory: /home/salvatore/ros_ws/build/ros_eser1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
